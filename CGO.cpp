c169a77482857168e56a4e56fb61531424eae15b
Khademnia <erfank@iastate.edu>
2024-09-25 22:02:49 -0500

    Changed network structure, and added CGO objects

---
+++ CGO.cpp
@@ -0,0 +1,5 @@
+//
+// Created by erfank on 9/24/2024.
+//
+
+#include "CGO.h"
---
+++ CGO.h
@@ -0,0 +1,16 @@
+//
+// Created by erfank on 9/24/2024.
+//
+
+#ifndef CGO_H
+#define CGO_H
+
+
+
+class CGO {
+
+};
+
+
+
+#endif //CGO_H
